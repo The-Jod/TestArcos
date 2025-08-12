295c04a34d4d2415c8ad657959075e92d3f417d5
TheJod <31497609+The-Jod@users.noreply.github.com>
2025-08-12 11:04:56 -0400

    Update winget upgrade.ps1

--- winget upgrade.ps1
+++ winget upgrade.ps1
@@ -1,9 +1,15 @@
-winget upgrade --all --silent
-
-if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
-    Install-Module PSWindowsUpdate -Force -Confirm:$false -Scope CurrentUser
+# Verifica si winget está disponible
+if (Get-Command winget -ErrorAction SilentlyContinue) {
+    winget upgrade --all --silent
+} else {
+    Write-Output "Winget no está disponible en este equipo."
 }
 
-Import-Module PSWindowsUpdate
-Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+# Verifica si el módulo PSWindowsUpdate está instalado
+if (Get-Module -ListAvailable -Name PSWindowsUpdate) {
+    Import-Module PSWindowsUpdate
+    Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+} else {
+    Write-Output "El módulo PSWindowsUpdate no está instalado."
+}
 
