ebdfc00c1fa62c25d7892856d1d65917b862b3e8
TheJod <31497609+The-Jod@users.noreply.github.com>
2025-08-12 10:59:25 -0400

    Update winget upgrade.ps1

--- winget upgrade.ps1
+++ winget upgrade.ps1
@@ -1,4 +1,9 @@
-winget upgrade
-y
-winget upgrade --all
-pause
+winget upgrade --all --silent
+
+if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
+    Install-Module PSWindowsUpdate -Force -Confirm:$false -Scope CurrentUser
+}
+
+Import-Module PSWindowsUpdate
+Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+
